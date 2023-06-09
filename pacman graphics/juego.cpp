9760025e259c7b7cf5254b417cd96c37f865db6a
Uriel <uespinosa770@hotmail.com>
2023-06-08 19:14:32 -0600

    Ya dejo de aparecer la marca de las colisiones y se agrego una mas, falta agregar colisiones al spawn

--- pacman graphics/juego.cpp
+++ pacman graphics/juego.cpp
@@ -37,10 +37,10 @@ void juego::dibujar_pantalla()
 	ventana1->draw(smapa);
    
 	
-	for (auto& colision : colisiones )
+	/*for (auto& colision : colisiones )
         {
             ventana1->draw(colision);
-        }
+        }*/
         for (auto& past : spastillas) {
             ventana1->draw(past);
         }
@@ -76,11 +76,10 @@ void juego::gameLoop(){
 			redPosition=colision(sredit1,coli,redPosition);
 			redPosition=colision(sredit2,coli,redPosition);
 			redPosition=colision(sredit3,coli,redPosition);
-			redPosition=colision(sredit4,coli,redPosition);
+			
 			wumPosition=colision(swumpus1,coli,wumPosition);
 			wumPosition=colision(swumpus2,coli,wumPosition);
 			wumPosition=colision(swumpus3,coli,wumPosition);
-			colplayer(sredit1,swumpus1);
 		}	
 		
 		Puntos(sredit1,1);
@@ -113,12 +112,12 @@ void juego::llenar_cords(){
 	tamano={{660,16},{15,730},{660,11},{11,730},{131,252},{133,252},{38,110},{60,36},{60,36},
 		{108, 60},{83, 60},{83, 36},{108, 58},{83, 60},{83, 36},{34, 182},{34, 180},{180, 36},
 		{34, 110},{34, 110},{180, 34},{108, 35},{83, 35},{108, 35},{83, 35},{180, 34},{228, 35},
-		{228, 35},{70, 34},{70, 34},{34, 70},{34, 70},{35, 107},{35, 107},{34, 107},{34, 107}};
+		{228, 35},{70, 34},{70, 34},{34, 70},{34, 70},{35, 107},{35, 107},{34, 107},{34, 107},{34, 107}};
 	
-	cords={{105,130},{105,130},{105,850},{753,130},{635,348},{105,348},{417,130},{105,708},{706,708},
+	cords={{105,130},{105,130},{105,855},{753,130},{635,348},{105,348},{417,130},{105,708},{706,708},
 	       {490, 179},{635, 180},{635, 275},{275, 180},{155, 180},{155, 275},{563, 275},{275, 275},{347, 275},
 	       {563,490},{275,490},{347, 565},{490, 637},{635, 637},{275, 637},{155, 637},{347, 709},{490, 780},
-	       {155, 780},{490, 349},{312, 349},{420, 312},{420, 600},{635, 637},{203, 637},{563,708},{275,708}};
+	       {155, 780},{490, 349},{312, 349},{420, 312},{420, 600},{635, 637},{203, 637},{563,708},{275,708},{420,708}};
 
 
 }
