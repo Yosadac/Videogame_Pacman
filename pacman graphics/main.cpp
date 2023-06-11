2c7cf352cd91700487a020ec27a1464b0aa3526d
Uriel <uespinosa770@hotmail.com>
2023-06-11 16:30:39 -0600

    Ya funciona el menu

--- pacman graphics/main.cpp
+++ pacman graphics/main.cpp
@@ -1,20 +1,9 @@
 #include "juego.h"
 #include "menu.h"
 
-//#include <SFML/Graphics.hpp>
-
-//using namespace sf;
-
-/*int main (int argc,char * args[]){
-
-		juego * partida1;
-		mostrarMenu();
-		partida1 = new juego(1000,1000,"Intento");
-	return 0;
-}*/
-
 int main(int argc, char* args[]) {
-    mostrarMenu();
+
+	mostrarMenu();
     juego* partida1 = new juego(1000, 1000, "Bite The Bytes");
     return 0;
 }
