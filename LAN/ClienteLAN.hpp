3a452c0c45b1d95dfa6ce139885ec393c4e3b7b0
Enryse-Doors <65697749+Enryse-Doors@users.noreply.github.com>
2023-06-13 21:02:12 -0600

    Update ClienteLAN.hpp

--- LAN/ClienteLAN.hpp
+++ LAN/ClienteLAN.hpp
@@ -6,9 +6,19 @@
 
 class ClienteLAN {
     public:
+        /**
+         * Conectar con la sala servidor
+         *
+         * Se conecta a la sala creada por el servidor especificador.
+         * Es necesario que el servidor haya sido inicializado primero.
+         * Una vez realizada la conexión, se podrán usar socketIn y socketOut para enviar y recibir mensajes respectivamente.
+         *
+         * @param ipServer Dirección IP del servidor al que conectarse.
+         * @return Indicador de exito al completar la función.
+         */
         int ConectarseASala(sf::IpAddress ipServer);
+        sf::TcpSocket socketIn; //Socket de entrada de mensajes con el servidor
+        sf::TcpSocket socketOut; //Socket de salida de mensajes con el servidor
     protected:
-        sf::TcpSocket socketIn;
-        sf::TcpSocket socketOut;
         int EsperarConexion();
 };
