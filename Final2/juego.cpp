974337d188b156806b9d67326fb2d900b2eb2c36
Pusheen <83900686+Yosadac@users.noreply.github.com>
2023-06-14 19:46:46 -0600

    Add files via upload

---
+++ Final2/juego.cpp
@@ -0,0 +1,364 @@
+#include "juego.h"
+#include "menu.h"
+#include <asio.hpp>
+#include <iostream>
+#include <thread>
+#include <fstream>
+#include <SFML/Network.hpp>
+#include <csignal>
+#include <cstring>
+#include <unistd.h>
+#include <string>
+
+
+
+juego::juego(int resolucion_x,int resolucion_y, string titulo){
+
+	fps=60;
+	mov_wum=0;
+	mov_red=0;
+	iniciar=0;
+	diferenciaRed=0;
+	powerupred=false;
+	RedClock=1;
+	diferenciaWum=0;
+	powerupwum=false;
+	WumClock=1;
+	redLife=3;
+	wumLife=3;
+	confirmationRed=false;
+	confirmationWum=false;
+	diferenciaTux=0;
+	poweruptux=false;
+	TuxClock=1;
+	tuxLife=3;
+	confirmationTux=false;
+	diferenciaAnd=0;
+	powerupand=false;
+	AndClock=1;
+	andLife=3;
+	confirmationAnd=false;
+	players_alive=4;
+	red_alive=true;
+	wum_alive=true;
+	tux_alive=true;
+	and_alive=true;
+
+	ContextSettings settings;
+	settings.antialiasingLevel = 0;
+	ventana1 = new RenderWindow(VideoMode(resolucion_x,resolucion_y),titulo);
+	ventana1->setFramerateLimit(fps);
+
+
+	//connection();
+	
+	 std::ofstream archivo("status.txt");
+    if (archivo.is_open()) {
+        		// Escribir la dirección IP en el archivo
+        		archivo << "0";
+        		archivo.close();
+        		std::cout << "status actualizado" << std::endl;
+    } 
+
+    cargar_texturas();
+    //crear_colision_jugador();
+	llenar_cords();
+	crear_colisiones();
+	crear_pastillas();
+	//spastilla.setPosition(300,300);
+	
+			
+	gameLoop();
+
+}
+
+
+
+
+
+void juego::dibujar_pantalla()
+{    	
+    ventana1->clear();
+	if(players_alive==1){
+		winner();
+	}
+	else{
+
+		ventana1->draw(smapa);
+	
+
+		/*for (auto& colision : colisiones )
+	        {
+	            ventana1->draw(colision);
+	        }*/
+	    for (auto& past : spastillas) {
+	        ventana1->draw(past);
+	    }
+
+		if(redLife>0){
+			LastScoreRed=score_red-diferenciaRed;
+			if(LastScoreRed==100 && confirmationWum==false){
+				powerupred=true;
+				confirmationRed=true;
+			}
+			Power_up(LastScoreRed,diferenciaRed,RedClock,clockRed,TiempoRed,secondsRed,TextTimeRed,powerupred,1,score_red,confirmationRed);
+		}
+		if(wumLife>0){
+			LastScoreWum=score_wum-diferenciaWum;
+			if(LastScoreWum==100 && confirmationRed==false){
+				powerupwum=true;
+				confirmationWum=true;
+			}
+			Power_up(LastScoreWum,diferenciaWum,WumClock,clockWum,TiempoWum,secondsWum,TextTimeWum,powerupwum,2,score_wum,confirmationWum);
+		}
+		if(tuxLife>0){
+			LastScoreTux=score_tux-diferenciaTux;
+			if(LastScoreTux==100 && confirmationTux==false){
+				poweruptux=true;
+				confirmationTux=true;
+			}
+			Power_up(LastScoreTux,diferenciaTux,TuxClock,clockTux,TiempoTux,secondsTux,TextTimeTux,poweruptux,3,score_tux,confirmationTux);
+		}
+		if(andLife>0){
+			LastScoreAnd=score_and-diferenciaAnd;
+			if(LastScoreAnd==100 && confirmationAnd==false){
+				powerupand=true;
+				confirmationAnd=true;
+			}
+			Power_up(LastScoreAnd,diferenciaAnd,AndClock,clockAnd,TiempoAnd,secondsAnd,TextTimeAnd,powerupand,4,score_and,confirmationAnd);
+		}
+    	mostrar_score();
+		//Puntos();
+ 	    //animacion_pacman_muerte();
+    	//ventana1->draw(sred);
+	}
+		ventana1->display();
+	
+}
+
+void juego::procesar_eventos(){
+
+	while (ventana1->pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+            
+                ventana1->close();
+            }
+        }
+
+        /*mov_player2();
+        mov_player3();
+        mov_player4();*/
+        
+        std::ifstream archivo("player.txt");
+	std::string status;
+	std::getline(archivo, status);
+	archivo.close();
+	trim(status);
+        
+        if(status == "player1"){
+        mov_redit();
+        }
+        else{
+        mov_redit1();
+        }
+        if(status == "player2"){
+        mov_wumpus();
+        }
+        else{
+        mov_wumpus1();
+        }
+        
+        
+        //mov_redit();
+        //mov_wumpus();
+		mov_tuxf();
+		mov_android();
+}
+
+
+
+
+
+void juego::gameLoop(){
+
+	
+	// Abrir el archivo "direccion.txt" en modo de lectura
+    std::ifstream archivo("direccion.txt");
+    std::string contenido;
+
+    if (archivo.is_open()) {
+        // Leer el contenido del archivo y guardarlo en la variable "contenido"
+        std::getline(archivo, contenido);
+
+        archivo.close();
+        //std::cout << "Contenido del archivo direccion.txt: " << contenido << std::endl;
+    }
+	
+	trim(contenido);
+	
+	sf::TcpSocket socket;
+    	sf::IpAddress ip(contenido); // IP de destino en la red local
+	 unsigned short port = 2000;
+	 
+	  if (socket.connect(ip, port) != sf::Socket::Done) {
+        std::cerr << "Error al conectar al servidor" << std::endl;
+    	}
+    	
+    	
+
+
+	while(ventana1->isOpen()){	
+	
+		std::string message; 
+		 if (Keyboard::isKeyPressed(Keyboard::Left))
+        	{
+             	message = "Tecla A presionada";
+        	socket.send(message.c_str(), message.size() + 1);
+        	}
+        	else if (Keyboard::isKeyPressed(Keyboard::Right))
+        	{
+           	message = "Tecla D presionada";
+        	socket.send(message.c_str(), message.size() + 1);
+        	}
+        	else if (Keyboard::isKeyPressed(Keyboard::Up))
+        	{
+            	message = "Tecla W presionada";
+        	socket.send(message.c_str(), message.size() + 1);
+        	}
+        	else if (Keyboard::isKeyPressed(Keyboard::Down))
+        	{
+             	message = "Tecla S presionada";
+        	socket.send(message.c_str(), message.size() + 1);
+        	}
+	
+	
+	
+		
+		procesar_eventos();
+		dibujar_pantalla();
+		for(auto& coli : colisiones){
+			redPosition=colision(sredit1,coli,redPosition);
+			redPosition=colision(sredit2,coli,redPosition);
+			redPosition=colision(sredit3,coli,redPosition);
+			redPosition=colision(sredit4,coli,redPosition);
+			
+			wumPosition=colision(swumpus1,coli,wumPosition);
+			wumPosition=colision(swumpus2,coli,wumPosition);
+			wumPosition=colision(swumpus3,coli,wumPosition);
+
+			tuxPosition=colision(stux1,coli,tuxPosition);
+			tuxPosition=colision(stux2,coli,tuxPosition);
+			tuxPosition=colision(stux3,coli,tuxPosition);
+			tuxPosition=colision(stux4,coli,tuxPosition);
+
+			andPosition=colision(sandroid1,coli,andPosition);
+			andPosition=colision(sandroid2,coli,andPosition);
+			andPosition=colision(sandroid3,coli,andPosition);
+			andPosition=colision(sandroid4,coli,andPosition);
+
+		}	
+		
+		Puntos(sredit1,1);
+		Puntos(sredit2,1);
+		Puntos(sredit3,1);
+		Puntos(sredit4,1);
+		
+		Puntos(swumpus1,2);
+		Puntos(swumpus2,2);
+		Puntos(swumpus3,2);
+
+		Puntos(stux1,3);
+		Puntos(stux2,3);
+		Puntos(stux3,3);
+		Puntos(stux4,3);
+
+		Puntos(sandroid1,4);
+		Puntos(sandroid2,4);
+		Puntos(sandroid3,4);
+		Puntos(sandroid4,4);
+
+
+		
+		if(iniciar==0){
+			inicio.play();
+			while (inicio.getStatus() == sf::SoundSource::Playing) {
+	    			// esperar un poco antes de volver a comprobar el estado
+			    	sleep(milliseconds(100));
+			    	iniciar=1;
+			}
+		}
+		
+	}
+
+}
+
+
+
+
+
+void juego::llenar_cords(){
+
+	tamano={{660,16},{15,730},{660,11},{11,730},{131,252},{133,252},{38,110},{60,36},{60,36},
+		{108, 60},{83, 60},{83, 36},{108, 58},{83, 60},{83, 36},{34, 182},{34, 180},{180, 36},
+		{34, 110},{34, 110},{180, 34},{108, 35},{83, 35},{108, 35},{83, 35},{180, 34},{228, 35},
+		{228, 35},{70, 34},{70, 34},{34, 70},{34, 70},{35, 107},{35, 107},{34, 107},{34, 107},{34, 107},
+		{60,15},{15,110},{182,15},{15,110},{60,15}};
+	
+	cords={{105,130},{105,130},{105,855},{753,130},{635,348},{105,348},{417,130},{105,708},{706,708},
+	    {490, 179},{635, 180},{635, 275},{275, 180},{155, 180},{155, 275},{563, 275},{275, 275},{347, 275},
+	    {563,490},{275,490},{347, 565},{490, 637},{635, 637},{275, 637},{155, 637},{347, 709},{490, 780},
+	    {155, 780},{490, 349},{312, 349},{420, 312},{420, 600},{635, 637},{203, 637},{563,708},{275,708},{420,708},
+		{345,420},{345,420},{345,515},{513,420},{467,420}};
+
+
+}
+
+
+
+
+
+
+void juego::crear_colisiones(){
+	
+	int col;
+	// Crear 30 colisiones aleatorias
+	for (int i = 0; i <= 9; i++)
+	{
+      	// Crear un rectángulo con una posición y tamaño aleatorios
+       	RectangleShape colision(Vector2f(tamano[i].first,tamano[i].second));
+       	colision.setPosition(cords[i].first, cords[i].second);
+       	colision.setFillColor(Color::Transparent);
+       	colision.setOutlineThickness(1);
+    	colision.setOutlineColor(Color::Red);
+
+       	// Agregar el rectángulo al vector de colisiones
+       	colisiones.push_back(colision);
+       	
+       	col=i;
+   	}
+    	
+  	for(col;col<=41;col++){
+   		RectangleShape obs(Vector2f(tamano[col].first,tamano[col].second));
+       	obs.setPosition(cords[col].first, cords[col].second);
+       	obs.setFillColor(Color::Transparent);
+       	obs.setOutlineThickness(1);
+   		obs.setOutlineColor(Color::Green);
+
+       	// Agregar el rectángulo al vector de colisiones
+       	colisiones.push_back(obs);
+    	
+   	}	
+}
+
+
+
+void juego::Texto_time(string texto, int tam_x, int tam_y){
+
+	Text TimeRemaining(texto, font, 20);
+    TimeRemaining.setPosition(tam_x, tam_y);    
+	ventana1->draw(TimeRemaining);
+}
+
+
---
+++ Final2/juego.h
@@ -0,0 +1,112 @@
+//#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+//#include <gif_lib.h>
+#include <vector>
+#include <iostream>
+#include <SFML/Network.hpp>
+
+using namespace std;
+using namespace sf;
+
+
+class juego{
+
+	public:
+	
+		juego(int resolucion_x,int resolucion_y, string titulo);
+		void trim(std::string& str);
+		void connection();
+		void establecerConexionClient();
+		
+		void enviar();
+		void recibir();
+		
+		void recibirMensajes(sf::TcpSocket& socket);
+		bool establecerConexion(sf::TcpListener& listener, sf::TcpSocket& socket, unsigned short port);
+		void enviarMensajes();
+		void dibujar_pantalla();
+		void gameLoop();
+		void llenar_cords();
+		void procesar_eventos();
+		void detectar_colision();
+		void cargar_texturas();
+		void crear_colisiones();
+		void Puntos(Sprite jugador, int flag);
+		void mostrar_score();
+		void crear_pastillas();
+		void posicion_puntos();
+		void Anim_pastillas();
+		void colplayer(Sprite &x , Sprite &y, Vector2f &Position,int flag, int &lifes);
+		
+		void animacion_redit();
+		void mov_redit();
+		void mov_redit1();
+		
+		void animacion_wumpus();
+		void mov_wumpus();
+		void mov_wumpus1();
+
+		void animacion_tux();
+		void mov_tuxf();
+
+		void animacion_android();
+		void mov_android();
+
+		void Texto_time(string texto, int tam_x, int tam_y);
+
+		void Power_up(int &LastScore,int &diferencia,int &clockInd,Clock &clocktime, Time &Tiempo,int &seconds, string &TextTime,bool &powerupplayer, int flag, int score, bool &confirmation);
+		
+		void winner();
+
+		Vector2f colision(Sprite x, RectangleShape Obs1, Vector2f spritePosition);
+	private:
+	
+		RenderWindow * ventana1;
+		int fps,mov,iniciar,mov_red,mov_wum,mov_tux,mov_and;
+		
+		Font font;
+		Texture mapa;
+		Sprite smapa;
+		
+		Texture mpac1,mpac2,mpac3,mpac4,mpac5,mpac6,mpac7,mpac8,mpac9,mpac10,mpac11,mpac12,pastilla;
+		Sprite smpac1,smpac2,smpac3,smpac4,smpac5,smpac6,smpac7,smpac8,smpac9,smpac10,smpac11,smpac12;
+		
+		Texture bola1;
+		
+		Texture redit1,redit2,redit3,redit4,wumpus1,wumpus2,wumpus3,tux1,tux2,tux3,tux4,android1,android2,android3,android4;
+		
+		Sprite sredit1,sredit2,sredit3,sredit4,swumpus1,swumpus2,swumpus3,stux1,stux2,stux3,stux4,sandroid1,sandroid2,sandroid3,sandroid4;
+		
+		Sprite demo_redit1,demo_redit2, demo_redit3, demo_wumpus1, demo_wumpus2, demo_wumpus3,demo_tux1,demo_tux2,demo_tux3,demo_tux4,demo_android1,demo_android2,demo_android3,demo_android4;
+		
+		vector<Sprite> spastillas;	
+		
+		Event event;
+		Vector2f redPosition,wumPosition,tuxPosition,andPosition;
+		int spriteIndex,aviso_red,aviso_wum,aviso_tux,aviso_android;
+		vector<RectangleShape> colisiones;
+		vector<pair<int,int>> tamano;
+		vector<pair<int,int>> cords;
+		vector<pair<int,int>> cords_pastillas;
+		Clock clock;
+		//Text scoreText_red,scoreText_wam;
+		
+		std::string scoreString_red, scoreString_wum, scoreString_tux,scoreString_and, TextTimeRed, TextTimeWum, TextTimeTux, TextTimeAnd;
+		int score_red,score_wum,score_tux,score_and;
+		Sound sound,inicio,soundpowerup,soundpowerdown;
+		SoundBuffer buffer,bufferInicio,bufferpowerup,bufferpowerdown;
+		
+		
+		Time TiempoRed,TiempoWum,TiempoTux,TiempoAnd;
+    	int secondsRed,secondsWum,secondsTux,secondsAnd;
+    	Clock clockRed,clockWum,clockTux,clockAnd;
+
+		int diferenciaRed,RedClock,LastScoreRed,LastScoreWum,diferenciaWum, WumClock,redLife,wumLife;
+		int diferenciaTux,TuxClock,LastScoreTux,diferenciaAnd,AndClock,LastScoreAnd,tuxLife,andLife;
+		bool powerupred,powerupwum,poweruptux,powerupand;
+		bool confirmationRed,confirmationWum,confirmationTux,confirmationAnd;
+
+		int players_alive;
+		bool red_alive,wum_alive,tux_alive,and_alive;
+};
